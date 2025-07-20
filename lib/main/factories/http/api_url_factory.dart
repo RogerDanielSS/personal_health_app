0c850e47b07ace20757ada1a6025cebe6a234563
rogerdanielss <danielsimoesp@outlook.com>
2025-07-20 16:38:11 -0300

    chore: use navigator localhost

--- lib/main/factories/http/api_url_factory.dart
+++ lib/main/factories/http/api_url_factory.dart
@@ -1,3 +1,5 @@
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-String makeApiUrl(String path) => 'http://10.0.2.2:3000/$path';
+// String makeApiUrl(String path) => 'http://10.0.2.2:3000/$path';
+String makeApiUrl(String path) => 'http://127.0.0.1:3000/$path';
+
