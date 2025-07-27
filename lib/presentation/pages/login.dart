a239e2a3d1923a49f7c930b05338eb2fe3dbacdc
rogerdanielss <danielsimoesp@outlook.com>
2025-07-27 13:15:30 -0300

    feat: improve login page design

--- lib/presentation/pages/login.dart
+++ lib/presentation/pages/login.dart
@@ -47,64 +47,77 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
+      body: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey.shade50,
+            image: DecorationImage(
+              image: AssetImage(
+                  'assets/background_1920x1080.png'), // Local image
+              fit: BoxFit.cover,
+            ),
+          ),
           padding: const EdgeInsets.all(16.0),
+          alignment: Alignment.center,
           child: Column(
+            spacing: 40,
             children: [
               ClipOval(
                 child: Image.asset(
-                  'assets/logo.png',
+                  'assets/logo_128.png',
                   height: 200,
                 ),
               ),
               Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextFormField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Insira seu email';
-                        }
-                        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                          return 'Enter a valid email';
-                        }
-                        return null;
-                      },
+                  key: _formKey,
+                  child: Container(
+                    constraints: BoxConstraints(maxWidth: 400),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Insira seu email';
+                            }
+                            if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                .hasMatch(value)) {
+                              return 'Enter a valid email';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 16),
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            labelText: 'Senha',
+                            border: OutlineInputBorder(),
+                          ),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            if (value.length < 6) {
+                              return 'Password must be at least 6 characters long';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 24),
+                        ElevatedButton(
+                          onPressed: () => _login(context),
+                          child: Text('Entrar'),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 16),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        labelText: 'Senha',
-                        border: OutlineInputBorder(),
-                      ),
-                      obscureText: true,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your password';
-                        }
-                        if (value.length < 6) {
-                          return 'Password must be at least 6 characters long';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 24),
-                    ElevatedButton(
-                      onPressed: () => _login(context),
-                      child: Text('Entrar'),
-                    ),
-                  ],
-                ),
-              ),
+                  )),
             ],
           )),
     );
